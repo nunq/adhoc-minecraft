6967a11801092673769547b970683fcef4fd6ff9
hyphenc <46054695+nunq@users.noreply.github.com>
2024-05-18 14:13:50 +0200

    add cli args to configure external storage access

--- create.sh
+++ create.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+set -eu -o pipefail
+
+while getopts u:p:s: flag
+do
+    case "${flag}" in
+        u) username=${OPTARG};;
+        p) pass=${OPTARG};;
+        s) storage=${OPTARG};;
+    esac
+done
 
 terraform apply -auto-approve
 
@@ -10,5 +20,6 @@ sleep 20
 ansible-playbook -i "$IP," \
   --private-key ~/.ssh/id_ed25519_adhoc-mc \
   -u root \
+  --extra-vars "ext_storage=$storage user=$username pass=$pass" \
   ansible/create-minecraft.yml
 
