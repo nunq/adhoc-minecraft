83cfa9b9e2645f9f6c2cd6203f3b4fbecdd5367a
hyphenc <46054695+nunq@users.noreply.github.com>
2024-05-18 14:26:13 +0200

    auto-create ssh key for adhoc mc

--- create.sh
+++ create.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
+SSH_KEY_PATH="~/.ssh/id_ed25519_adhoc-mc"
+
 while getopts u:p:s: flag
 do
-    case "${flag}" in
-        u) username=${OPTARG};;
-        p) pass=${OPTARG};;
-        s) storage=${OPTARG};;
-    esac
+  case "${flag}" in
+    u) username=${OPTARG};;
+    p) pass=${OPTARG};;
+    s) storage=${OPTARG};;
+  esac
 done
+
+if [ ! -f "$SSH_KEY_PATH" ]; then
+  echo  "Creating ad-hoc minecraft SSH key ..."
+  ssh-keygen -t ed25519 -f "$SSH_KEY_PATH" -N "" -C "adhoc-mc"
+fi
 
 terraform apply -auto-approve
 
